358b59160ebe09386759960ec73983361fac460f
maggyleet <mahaengineer7@gmail.com>
2025-02-01 19:17:06 +0530

    Update and Test the code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((num, total) => num + total , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((num, total) => num * total , 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+	while(num>0){
+    fact*=num--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let c;
+    if (typeof n!== 'number') {
+        c = parseInt(n);
+    } else {
+        c = n;
+    }
+    //let c =(typeOf n !== 'number') ? parseInt(n) : n;
+
+    if (c < 0) return "OOPS";
+    if (c == 0) return 0;
+
+    let f1 = 1;
+    let f2 = 0;
+
+    for (let i = 2; i <= c; i++) {
+        let curr = f1 + f2;
+        f2 = f1;
+        f1 = curr;
+    }
+
+    return f1;
 
 };
 
